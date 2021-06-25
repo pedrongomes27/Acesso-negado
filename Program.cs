d1196f97d86946ca6b9d6caa81e27669ddb0fa35
pedrongomes27 <pedro.ngomes27@hotmail.com>
2021-06-25 10:29:48 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acesso_negado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = Console.ReadLine();
+            while (senha != "admin123")
+            {
+                Console.WriteLine("Senha incorreta, por favor tente novamente");
+                senha = Console.ReadLine();
+            }
+
+            Console.WriteLine("Senha Correta");
+            Console.ReadKey();
+
+        }
+    }
+}
